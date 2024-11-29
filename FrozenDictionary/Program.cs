47ce54b39a79be0461e32baa177a0c7143f35855
krzysztof-lata <krzysztof.lata.kl@gmail.com>
2024-11-29 01:05:37 +0100

    FrozenDictionary StructAllocation

---
+++ FrozenDictionary/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using FrozenDictionary;
+
+BenchmarkRunner.Run<Benchmark>();
---
+++ StructAllocation/Program.cs
@@ -0,0 +1,23 @@
+﻿Create();
+
+void Create()
+{
+    string[] list = ["abc", null, "def"];
+    var ofType = list.OfType<string>();
+    //var testStruct = new TestStruct(10_000);
+    Console.ReadKey();
+}
+
+public struct TestStruct
+{
+    public int[] Data;
+
+    public TestStruct(int size)
+    {
+        Data = stackalloc int[size].ToArray();
+        for (int i = 0; i < Data.Length; i++)
+        {
+            Data[i] = i;
+        }
+    }
+}
