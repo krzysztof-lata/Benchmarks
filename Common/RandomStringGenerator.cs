3620cdf00361135587c9afe780cb663d11d2fe42
krzysztof-lata <krzysztof.lata.kl@gmail.com>
2025-01-26 17:38:41 +0100

    RandomStringGenerator

---
+++ Common/RandomStringGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Common;
+
+public class RandomStringGenerator
+{
+    private readonly Random _random;
+    private const string Chars = "abcdefghijklmnopqrst";
+
+    public RandomStringGenerator(int seed)
+    {
+        _random = new Random(seed);
+    }
+
+    public string Create(int length)
+    {
+        return new string(Enumerable.Range(1, length).Select(_ => Chars[_random.Next(Chars.Length)]).ToArray());
+    }
+}
